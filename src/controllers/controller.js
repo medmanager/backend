1b58c5ef0384016e14022fabc49ee8724589471b
dburns37 <dburns37@gatech.edu>
2021-01-21 10:55:35 -0500

    added basic get, post, put, and delete routes to manipulate medications

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -12,3 +12,49 @@ export const addNewMedication = (req, res) => {
         res.json(medication);
     });
 };
+
+export const getMedications = (req, res) => {
+    Medication.find({}, (err, medications) => {
+        if (err) res.send(err);
+        res.json(medications);
+    });
+};
+
+export const getMedicationFromID = (req, res) => {
+    Medication.findById(req.params.medicationID, (err, medication) => {
+        if (err) res.send(err);
+        res.json(medication);
+    });
+};
+
+export const updateMedicationFromID = (req, res) => {
+    Medication.findOneAndUpdate({_id: req.params.medicationID}, req.body, {new: true, useFindAndModify: false}, (err, medication) => {
+        if (err) res.send(err);
+        res.json(medication);
+    });
+};
+
+export const deleteMedicationFromID = (req, res) => {
+    Medication.remove({_id: req.params.medicationID}, (err, medication) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json({ message: 'successfully deleted medication'});
+    });
+};
+
+export const getTimesFromMedicationID = (req, res) => {
+    //{times: 1} ensures the function will only return the times for that medication
+    Medication.findById({_id: req.params.medicationID}, {times: 1},(err, times) => {
+        if (err) res.send(err);
+        res.json(times);
+    });
+};
+
+export const getTimeFromTimeID = (req, res) => {
+    //{times: {elemMatch...}} ensures the function will only return one time with the matching _id
+    Medication.find({"times._id":req.params.timeID}, {times : {$elemMatch: {"_id": req.params.timeID}}},(err, time) => {
+        if (err) res.send(err);
+        res.json(time);
+    });
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,29 @@
-import { addNewMedication } from '../controllers/controller'
+import { addNewMedication, getMedications, 
+         getMedicationFromID, updateMedicationFromID, deleteMedicationFromID,
+         getTimesFromMedicationID, getTimeFromTimeID} from '../controllers/controller'
 
 const routes = (app) => {
     app.route('/medication')
-        .post(addNewMedication);
+        .post(addNewMedication)
+        .get(getMedications);
+
+    app.route('/medication/:medicationID')
+        //get a specific medication from ID
+        .get(getMedicationFromID)
+        .put(updateMedicationFromID)
+        .delete(deleteMedicationFromID);
+
+    app.route('/medication/:medicationID/time')
+        //get update times of a specific medication
+        .get(getTimesFromMedicationID);
+        //TODO
+        //allow addition of a time
+
+    app.route('/medication/time/:timeID')
+        .get(getTimeFromTimeID);
+        //TODO
+        //changes to a time
+        //deletion of a time
 };
 
 export default routes;
