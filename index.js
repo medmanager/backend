a46144183f3f0d2b5587282191210391542e1497
peter-n-andersson <77506868+peter-n-andersson@users.noreply.github.com>
2021-02-01 10:14:00 -0500

    Login/Registration
    Login/registration services now included

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import routes from './src/routes/routes';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import jsonwebtoken from 'jsonwebtoken';
 
 const app = express();
 const PORT = 4000; //to run local
@@ -16,6 +17,20 @@ mongoose.connect('mongodb://localhost/MedManagerdb', {
 // bodyparser setup
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+// JWT setup
+app.use((req, res, next) => {
+    if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+        jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'ATLBANANA', (err, decode) => {
+            if (err) req.user = undefined;
+            req.user = decode;
+            next();
+        });
+    } else {
+        req.user = undefined;
+        next();
+    }
+})
 
 routes(app);
 
