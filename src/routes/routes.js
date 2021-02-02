2c25bcd08f47806a1ee99b3713db5ca89ee7f212
davidburns573 <davidburns573@gmail.com>
2021-02-01 23:53:50 -0500

    removed bugs

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,23 +5,23 @@ import {login, register, loginRequired} from '../controllers/UserControllers'
 
 const routes = (app) => {
 app.route('/medication')
-   .post(addNewMedication)
-   .get(getMedications);
+   .post(loginRequired, addNewMedication)
+   .get(loginRequired, getMedications);
 
 app.route('/medication/:medicationID')
    //get a specific medication from ID
-   .get(getMedicationFromID)
-   .put(updateMedicationFromID)
-   .delete(deleteMedicationFromID);
+   .get(loginRequired, getMedicationFromID)
+   .put(loginRequired, updateMedicationFromID)
+   .delete(loginRequired, deleteMedicationFromID);
 
 app.route('/medication/:medicationID/time')
    //get update times of a specific medication
-   .get(getTimesFromMedicationID);
+   .get(loginRequired, getTimesFromMedicationID);
    //TODO
    //allow addition of a time
 
 app.route('/medication/time/:timeID')
-   .get(getTimeFromTimeID);
+   .get(loginRequired, getTimeFromTimeID);
    //TODO
    //changes to a time
    //deletion of a time
