e5de27de394e9b30d94cb583cf316a88d60bc67a
samgamage <sammygamage@gmail.com>
2021-04-14 00:40:51 -0400

    Finalize seed

--- src/seed.js
+++ src/seed.js
@@ -7,10 +7,8 @@ import OccurrenceGroup from "./models/OccurrenceGroup";
 import Settings from "./models/Settings";
 import User from "./models/User";
 
-let reminderTimeSevenPM = new Date();
-reminderTimeSevenPM.setHours(19);
-let reminderTimeNinePM = new Date();
-reminderTimeNinePM.setHours(21);
+let reminderTimeSevenPM = new Date("April 14, 21 19:00:00");
+let reminderTimeNinePM = new Date("April 14, 21 21:00:00");
 
 let dosage7PM = {
     dose: 1,
@@ -34,6 +32,15 @@ let spironolactone = {
     frequency: {
         interval: 1,
         intervalUnit: "days",
+        weekdays: {
+            sunday: false,
+            monday: false,
+            tuesday: false,
+            wednesday: false,
+            thursday: false,
+            friday: false,
+            saturday: false,
+        },
     },
     dosages: [],
     color: 0,
@@ -49,6 +56,15 @@ let farxiga = {
     frequency: {
         interval: 1,
         intervalUnit: "days",
+        weekdays: {
+            sunday: false,
+            monday: false,
+            tuesday: false,
+            wednesday: false,
+            thursday: false,
+            friday: false,
+            saturday: false,
+        },
     },
     dosages: [],
     color: 1,
@@ -64,6 +80,15 @@ let aspirin = {
     frequency: {
         interval: 1,
         intervalUnit: "days",
+        weekdays: {
+            sunday: false,
+            monday: false,
+            tuesday: false,
+            wednesday: false,
+            thursday: false,
+            friday: false,
+            saturday: false,
+        },
     },
     dosages: [],
     color: 2,
@@ -79,6 +104,15 @@ let alendronate = {
     frequency: {
         interval: 1,
         intervalUnit: "days",
+        weekdays: {
+            sunday: false,
+            monday: false,
+            tuesday: false,
+            wednesday: false,
+            thursday: false,
+            friday: false,
+            saturday: false,
+        },
     },
     dosages: [],
     color: 3,
@@ -112,30 +146,34 @@ export const seed = async (req, res) => {
     const medOne = new Medication(spironolactone);
     const dosageOne = new Dosage(dosage9PM);
     await createOccurrences(dosageOne, 20);
-    medOne.dosages.push(dosageOne._id);
+    medOne.dosages.push(dosageOne);
     medOne.user = newUser._id;
     newUser.medications.push(medOne);
+    await createAndScheduleMedicationDosageOccurrences(medOne);
 
     const medTwo = new Medication(farxiga);
     const dosageTwo = new Dosage(dosage9PM);
     await createOccurrences(dosageTwo, 30);
-    medTwo.dosages.push(dosageTwo._id);
+    medTwo.dosages.push(dosageTwo);
     medTwo.user = newUser._id;
     newUser.medications.push(medTwo);
+    await createAndScheduleMedicationDosageOccurrences(medTwo);
 
     const medThree = new Medication(aspirin);
     const dosageThree = new Dosage(dosage9PM);
-    await createOccurrences(dosageThree, 15);
-    medThree.dosages.push(dosageThree._id);
+    await createOccurrences(dosageThree, 12);
+    medThree.dosages.push(dosageThree);
     medThree.user = newUser._id;
     newUser.medications.push(medThree);
+    await createAndScheduleMedicationDosageOccurrences(medThree);
 
     const medFour = new Medication(alendronate);
     const dosageFour = new Dosage(dosage7PM);
     await createOccurrences(dosageFour, 30);
-    medFour.dosages.push(dosageFour._id);
+    medFour.dosages.push(dosageFour);
     medFour.user = newUser._id;
     newUser.medications.push(medFour);
+    await createAndScheduleMedicationDosageOccurrences(medFour);
 
     await Promise.all([
         medOne.save(),
@@ -144,16 +182,12 @@ export const seed = async (req, res) => {
         medFour.save(),
         newUser.save(),
     ]);
-    await Promise.all([
-        createAndScheduleMedicationDosageOccurrences(medOne),
-        createAndScheduleMedicationDosageOccurrences(medTwo),
-        createAndScheduleMedicationDosageOccurrences(medThree),
-        createAndScheduleMedicationDosageOccurrences(medFour),
-    ]);
+
+    return res.status(200).json({ ok: true });
 };
 
 let timeTaken = new Date();
-timeTaken.setHours(0);
+timeTaken = new Date(timeTaken.getTime() - 24 * 3600 * 1000 * 2);
 
 const createOccurrences = async (dosage, trackingVal) => {
     for (let i = 0; i < 30; i++) {
